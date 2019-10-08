fb3ba803ca7423790a5535b01a6825a560ad8a75
Theo Wright <theowright2015@gmail.com>
2019-10-08 16:37:55 +0100

    completed log out

---
+++ firebase/2. firebase-login/firebase-login/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Authen from './Authen.js';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Authen />
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase/2. firebase-login/firebase-login/src/Authen.js
@@ -0,0 +1,119 @@
+import React, {Component} from 'react';
+
+let firebase = require('firebase');
+
+let config = {
+  apiKey: "AIzaSyAXfFEcd4hjauK7DcDtO41nMoDNKsqVQu4",
+  authDomain: "u-survey-a5394.firebaseapp.com",
+  databaseURL: "https://u-survey-a5394.firebaseio.com",
+  projectId: "u-survey-a5394",
+  storageBucket: "",
+  messagingSenderId: "421955787648",
+  appId: "1:421955787648:web:9fe511351a742c92ff7f65",
+  measurementId: "G-YWJCV8V818"
+};
+  firebase.initializeApp(config);
+
+
+
+class Authen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      err: ''
+    }
+
+    this.login = this.login.bind(this);
+    this.signUp = this.signUp.bind(this);
+    this.logOut = this.logOut.bind(this);
+  }
+
+
+
+
+  login(event) {
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    console.log(email, password);
+
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, password);
+
+    promise
+    .then( user => {
+      let lout = document.getElementById('logout');
+      lout.classList.remove('hide');
+      let error = "Welcome " + user.user.email;
+      this.setState({err: error})
+    })
+    .catch(e => {
+      let error = e.message;
+      console.log(error);
+      this.setState({err: error})
+    });
+  }
+
+
+
+
+  signUp() {
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+
+    promise
+    .then(user => {
+      let error = "Welcome " + user.user.email;
+      firebase.database().ref('users/' + user.user.uid).set({
+        email: user.user.email
+      // console.log(user);
+      // console.log(user.user.email);
+      });
+      // console.log(user);
+      this.setState({err: error})
+    })
+    .catch(e => {
+      let error = e.message;
+      // console.log(error);
+      this.setState(({err: error}))
+    });
+
+  }
+
+
+
+
+  logOut(event) {
+    firebase.auth().signOut();
+    let lout = document.getElementById('logout');
+    lout.classList.add('hide');
+    let message = "Thanks for coming";
+    this.setState({err: message})
+
+    document.getElementById('email').value="";
+    document.getElementById('pass').value="";
+  }
+
+
+
+  render() {
+    return(
+      <div>
+        <input id="email" type="email" ref="email" placeholder="Enter your email" />
+        <input id="pass" type="password" ref="password" placeholder="Enter your password" />
+        <br />
+        <p>{this.state.err}</p>
+
+        <button onClick={this.login}>Log In</button>
+        <button onClick={this.signUp}>Sign Up</button>
+        <button id="logout" className="hide" onClick={this.logOut}>Log Out</button>
+
+      </div>
+    )
+  }
+}
+
+export default Authen;
