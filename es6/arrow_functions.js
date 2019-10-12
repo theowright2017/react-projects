a7bfed99eaa915f62a87a9f2e3aa5ddf318e423d
Theo Wright <theowright2015@gmail.com>
2019-10-12 17:37:21 +0100

    commit

---
+++ es6/arrow_functions.js
@@ -0,0 +1,15 @@
+arrow = () => {
+  console.log("arrow");
+}
+
+arrow();
+
+
+///
+var addMe = () => {
+  let score= 200;
+  let bonus = 50;
+  return score + bonus;
+}
+
+console.log(addMe());
---
+++ es6/let_and_const.js
@@ -0,0 +1,15 @@
+if (true) {
+  var score = 80;
+  //let score = 80;
+}
+
+console.log(score);
+
+// var has global scope, let has function scope
+// let would mean score is undefined
+
+// let in and out of function would mean two seperate objects
+// whereas var would refer to same objectx
+
+
+/////////////////////////////
